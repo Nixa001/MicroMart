96125bfafed9dffd16ef2e138def48ea818edc37
nifaye <nicolasfaye31@gmail.com>
2024-07-27 13:44:34 +0000

    Feat: Init port for all services

--- services/product/cmd/main.go
+++ services/product/cmd/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Nixa001/micromart/pkg/common"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	serviceName := "product"
+	port := common.GetServicePort(serviceName)
+	router := gin.Default()
+
+	// Routes spécifiques au service...
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: router,
+	}
+
+	log.Printf("Starting %s service on port %s\n", serviceName, port)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
